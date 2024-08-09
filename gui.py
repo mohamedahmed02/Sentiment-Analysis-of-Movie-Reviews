081350d46cf7ba9c409848dd3c8ba6ba796e9f55
Muhammed Ahmed <87903508+mohamedahmed02@users.noreply.github.com>
2024-08-08 21:24:35 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -29,7 +29,11 @@ from io import StringIO
 import base64
 
 # Function to perform sentiment analysis
-
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
 def preprocess_text(text):
     # Remove URLs
     text = re.sub(r'http\S+', '', text)
